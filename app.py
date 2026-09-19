55a40393f3efccc62b80b75e21cc24b4cf19a0c7
RAKSHITH25 <rakshithshith2006@gmail.com>
2026-09-20 00:15:39 +0530

    Update application message

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ class Handler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header("Content-type", "text/html")
         self.end_headers()
-        self.wfile.write(b"Hello from CI/CD Docker Application!")
+        self.wfile.write(b"Hello from Automated CI/CD Pipeline!")
 
 server = HTTPServer(("0.0.0.0", 8000), Handler)
 
